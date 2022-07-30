2922b1f8f0c1fa480287c90e3eeeef5802f3993c
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-30 20:51:58 +0100

    Find constructor on the converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/converter/DefaultConverter.java
+++ src/main/java/com/otaviojava/converter/DefaultConverter.java
@@ -1,7 +1,10 @@
 package com.otaviojava.converter;
 
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 public class DefaultConverter implements Converter{
 
@@ -14,6 +17,27 @@ public class DefaultConverter implements Converter{
     @Override
     public <T> Map<String, Object> toMap(T entity) {
         Objects.requireNonNull(entity, "entity is required");
+        Map<String, Object> map = new HashMap<>();
+        String entityName = getEntityName(entity);
+        map.put("Entity", entityName);
+        Constructor<T> constructor = getConstructor(entity);
+        if (constructor != null) return constructor;
+
         return null;
+    }
+
+    private <T> Constructor<T> getConstructor(T entity) {
+        for (Constructor<?> constructor : entity.getClass().getDeclaredConstructors()) {
+            if(constructor.getParameterCount() == 0) {
+                return (Constructor<T>) constructor;
+            }
+        }
+        throw new IllegalStateException("There is no a legal constructor");
+    }
+
+    private static <T> String getEntityName(T entity) {
+        return Optional.ofNullable(entity.getClass().getAnnotation(Entity.class))
+                .map(Entity::value)
+                .orElse(entity.getClass().getName());
     }
 }
