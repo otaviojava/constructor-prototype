c572ab254986ee17deaa46c15cbee1a4d321d1cc
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-30 21:46:56 +0100

    Create test scenario to constructor
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/com/otaviojava/converter/DefaultConverterTest.java
+++ src/test/java/com/otaviojava/converter/DefaultConverterTest.java
@@ -1,5 +1,6 @@
 package com.otaviojava.converter;
 
+import com.otaviojava.converter.entities.Animal;
 import com.otaviojava.converter.entities.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +32,17 @@ class DefaultConverterTest {
         Assertions.assertEquals("Otavio", map.get("name"));
         Assertions.assertEquals("Salvador", map.get("city"));
         Assertions.assertEquals(Person.class.getSimpleName(), map.get("Entity"));
+    }
+
+    @Test
+    public void shouldReturnEntityUsingConstructor() {
+        Animal lion = Animal.builder().id("lion").race("mammal").name("Simba").build();
+        Map<String, Object> map = this.converter.toMap(lion);
+        Assertions.assertNotNull(map);
+        Assertions.assertEquals("lion", map.get("_id"));
+        Assertions.assertEquals("mammal", map.get("race"));
+        Assertions.assertEquals("Simba", map.get("name"));
+        Assertions.assertEquals(Animal.class.getSimpleName(), map.get("Entity"));
     }
 
 }
