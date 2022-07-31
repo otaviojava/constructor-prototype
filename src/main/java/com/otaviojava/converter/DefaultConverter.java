727f0f64923eebb1d63f4f8da342c45e8259558e
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-31 07:16:56 +0100

    Create beans map

--- src/main/java/com/otaviojava/converter/DefaultConverter.java
+++ src/main/java/com/otaviojava/converter/DefaultConverter.java
@@ -10,9 +10,21 @@ import java.util.function.Predicate;
 
 public class DefaultConverter implements Converter {
 
+    private final Map<String, Class<?>> beans = new HashMap<>();
+
+
+    public void add(Class<?> entity) {
+        Objects.requireNonNull(entity, "entity is required");
+        beans.put(getEntityName(entity), entity);
+    }
     @Override
     public <T> T toEntity(Map<String, Object> map) {
         Objects.requireNonNull(map, "map is required");
+        String entity = Optional.ofNullable(map.get("Entity"))
+                .map(Object::toString)
+                .orElseThrow(() -> new RuntimeException("The Entity key is mandatory"));
+
+
         return null;
     }
 
@@ -20,7 +32,7 @@ public class DefaultConverter implements Converter {
     public <T> Map<String, Object> toMap(T entity) {
         Objects.requireNonNull(entity, "entity is required");
         Map<String, Object> map = new HashMap<>();
-        String entityName = getEntityName(entity);
+        String entityName = getEntityName(entity.getClass());
         map.put("Entity", entityName);
         for (Field field : entity.getClass().getDeclaredFields()) {
             field.setAccessible(true);
@@ -89,8 +101,8 @@ public class DefaultConverter implements Converter {
                 .orElseThrow(() -> new IllegalStateException("There is no a legal constructor"));
     }
 
-    private <T> String getEntityName(T entity) {
-        return Optional.ofNullable(entity.getClass().getAnnotation(Entity.class))
+    private <T> String getEntityName(Class<T> entity) {
+        return Optional.ofNullable(entity.getAnnotation(Entity.class))
                 .map(Entity::value)
                 .filter(Predicate.not(String::isBlank))
                 .orElse(entity.getClass().getSimpleName());
