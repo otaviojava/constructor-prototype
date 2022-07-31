dd432cde095a1acfae51506cfb7357f9457fa6d9
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-31 21:05:25 +0100

    set value field
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/converter/FieldConstructor.java
+++ src/main/java/com/otaviojava/converter/FieldConstructor.java
@@ -2,6 +2,7 @@ package com.otaviojava.converter;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -12,24 +13,39 @@ import java.util.function.Predicate;
 public class FieldConstructor implements EntityConverter {
 
     @Override
-    public <T> T toEntity(Class<T> entity, Constructor<T> constructor, Map<String, String> map) {
-        T instance = constructor.newInstance();
+    public <T> T toEntity(Class<T> entity, Constructor<T> constructor, Map<String, Object> map) {
+        T instance = getInstance(constructor);
 
         for (Field field : entity.getDeclaredFields()) {
             Column column = field.getAnnotation(Column.class);
             Id id = field.getAnnotation(Id.class);
-            
-            if(id != null) {
-                
+
+            if (id != null) {
+
             } else if (column != null) {
                 field.setAccessible(true);
                 String name = Optional.of(column).map(Column::value)
                         .filter(Predicate.not(String::isBlank))
                         .orElse(field.getName());
-                String s = map.get(name);
+                Object value = map.get(name);
+                if (value != null) {
+                    try {
+                        field.set(instance, value);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
 
         }
         return null;
+    }
+
+    private static <T> T getInstance(Constructor<T> constructor) {
+        try {
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
