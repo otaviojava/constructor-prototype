811c6115ea9fe65935362901c783b4cde7ccc8ef
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-31 21:11:55 +0100

    Add load method on the converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/converter/Converter.java
+++ src/main/java/com/otaviojava/converter/Converter.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public interface Converter {
 
+    void add(Class<?> entity);
+
     <T> T toEntity(Map<String, Object> map);
 
     <T> Map<String, Object> toMap(T entity);
