5c70d05da367f27b37ba9f035db189504457719e
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-01 07:44:50 +0100

    remove extra code inside entitycreator consturtor
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/converter/EntityCreatorConstructor.java
+++ src/main/java/com/otaviojava/converter/EntityCreatorConstructor.java
@@ -14,47 +14,8 @@ public class EntityCreatorConstructor implements EntityCreator {
 
     @Override
     public <T> T toEntity(Class<T> entity, Constructor<T> constructor, Map<String, Object> map) {
-        T instance = getInstance(constructor);
 
-        for (Field field : entity.getDeclaredFields()) {
-            Column column = field.getAnnotation(Column.class);
-            Id id = field.getAnnotation(Id.class);
-            field.setAccessible(true);
-            if (id != null) {
-                field.setAccessible(true);
-                String name = Optional.of(id).map(Id::value)
-                        .filter(Predicate.not(String::isBlank))
-                        .orElse(field.getName());
-                setValue(map, instance, field, name);
-            } else if (column != null) {
-                field.setAccessible(true);
-                String name = Optional.of(column).map(Column::value)
-                        .filter(Predicate.not(String::isBlank))
-                        .orElse(field.getName());
-                setValue(map, instance, field, name);
-            }
-
-        }
-        return instance;
+        return null;
     }
 
-    private static <T> void setValue(Map<String, Object> map, T instance, Field field, String name) {
-        Object value = map.get(name);
-        if (value != null) {
-            try {
-                field.set(instance, value);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static <T> T getInstance(Constructor<T> constructor) {
-        try {
-            constructor.setAccessible(true);
-            return constructor.newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
 }
