99c919565b69c97de7b778f64005522247bb5ca3
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-01 07:31:54 +0100

    Add factory method on entity converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/converter/EntityConverter.java
+++ src/main/java/com/otaviojava/converter/EntityConverter.java
@@ -6,4 +6,13 @@ import java.util.Map;
 public interface EntityConverter {
 
     <T> T toEntity(Class<T> entity, Constructor<T> constructor, Map<String, Object> map);
+
+
+    static <T>  EntityConverter of(Constructor<T> constructor) {
+        if(constructor.getParameterCount() == 0) {
+            return new FieldConstructor();
+        }
+        return null;
+    }
+
 }
