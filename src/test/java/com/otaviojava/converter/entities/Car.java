554cae864311a0a23a22bc119e0bf86d083ee9a9
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-02 11:56:25 +0100

    Create car entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/test/java/com/otaviojava/converter/entities/Car.java
@@ -0,0 +1,21 @@
+package com.otaviojava.converter.entities;
+
+public class Car {
+
+    private String name;
+
+    private int year;
+
+    public Car(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public Car(int year) {
+        this.year = year;
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
