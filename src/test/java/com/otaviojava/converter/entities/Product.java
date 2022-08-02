7191d9d74df847bba5821f3ddb63768de7f4f7ba
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-02 12:07:23 +0100

    Create product
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/test/java/com/otaviojava/converter/entities/Product.java
@@ -0,0 +1,74 @@
+package com.otaviojava.converter.entities;
+
+import com.otaviojava.converter.Column;
+import com.otaviojava.converter.Entity;
+import com.otaviojava.converter.Id;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Product {
+
+    @Id
+    private final Long id;
+
+    @Column
+    private final String name;
+
+    @Column
+    private final String description;
+
+    @Column
+    private final BigDecimal price;
+
+    public Product(Long id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
